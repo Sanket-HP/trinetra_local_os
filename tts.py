e2f391eeb4cda5604c48b7e8fe1982848575f2d4
Sanket Patil <sp3130016@gmail.com>
2026-06-14 12:59:56 +0530

    Replace Termux voice and TTS

--- tts.py
+++ tts.py
@@ -1,9 +1,2 @@
-import os
-
 def speak(text):
     print("Trinetra:", text)
-
-    try:
-        os.system(f'termux-tts-speak "{text}"')
-    except:
-        pass
--- voice.py
+++ voice.py
@@ -1,24 +1,2 @@
-import subprocess
-
 def start_voice():
-
-    try:
-
-        result = subprocess.check_output(
-            [
-                "termux-speech-to-text"
-            ],
-            text=True
-        ).strip()
-
-        if not result:
-            return ""
-
-        if "ERROR" in result:
-            return ""
-
-        return result
-
-    except Exception as e:
-
-        return ""
+    return "hello trinetra"
