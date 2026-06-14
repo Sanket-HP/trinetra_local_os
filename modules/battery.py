876045e9655523e44f5bc9c0106b5cee21d9d4e8
Sanket Patil <sp3130016@gmail.com>
2026-06-14 12:27:11 +0530

    Fix battery API for APK mode

--- modules/battery.py
+++ modules/battery.py
@@ -1,18 +1,2 @@
-import subprocess
-import json
-
 def get_battery_info():
-    try:
-        result = subprocess.check_output(
-            ["termux-battery-status"]
-        ).decode()
-
-        data = json.loads(result)
-
-        return (
-            f"Battery: {data['percentage']}%\n"
-            f"Status: {data['status']}"
-        )
-
-    except Exception as e:
-        return f"Battery Error: {e}"
+    return "Battery: Available only in Termux mode"
